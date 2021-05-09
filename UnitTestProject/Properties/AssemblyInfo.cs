93d3a463d2dd2adefd57c1c192958e3f5df5eb12
Navpreet Singh <navpreets602@gmail.com>
2021-05-09 18:19:31 +1200

    Add project files.

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("52a30772-d8bf-4f31-8583-720d405148b8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using RentalVideoSystem;
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Database Obj_DB = new Database();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool data_Return = Obj_DB.AddRentedMovie("8", "100", DateTime.Today);
+            Assert.AreEqual(true, data_Return);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            bool data_Return = Obj_DB.AddRentedMovie("15", "10", DateTime.Today);
+            Assert.AreEqual(true, data_Return);
+        }
+    }
+}
